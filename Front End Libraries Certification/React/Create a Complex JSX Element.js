ccd9287af6d31ba9b707261562f88b318ff4cdd0
luclegen <luc.dev.sun@gmail.com>
2020-04-02 15:52:09 +0700

    Create a Complex JSX Element

---
+++ Front End Libraries Certification/React/Create a Complex JSX Element.js
@@ -0,0 +1,10 @@
+// write your code here
+const JSX = <div>
+                <h1></h1>
+                <p></p>
+                <ul>
+                    <li>One</li>
+                    <li>Two</li>
+                    <li>Three</li>
+                </ul>
+            </div>;
