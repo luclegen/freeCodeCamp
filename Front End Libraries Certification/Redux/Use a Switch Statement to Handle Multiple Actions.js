5fe300908bddeebbe00b5557cb18ef203f2d57cb
luclegen <luc.dev.sun@gmail.com>
2020-04-05 11:07:24 +0700

    Use a Switch Statement to Handle Multiple Actions

---
+++ Front End Libraries Certification/Redux/Use a Switch Statement to Handle Multiple Actions.js
@@ -0,0 +1,38 @@
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+  // change code below this line
+  // switch (action.type) {
+  //   case 'LOGIN':
+  //     return {authenticated: true};
+  //     break;
+  //   case 'LOGOUT':
+  //     return {authenticated: false};
+  //     break;
+  //   default:
+  //     return state;
+  //     break;
+  // }
+  return action.type === 'LOGIN'
+          ? {authenticated: true}
+          : action.type === 'LOGOUT'
+            ? {authenticated: false}
+            : state;
+  // change code above this line
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+};
